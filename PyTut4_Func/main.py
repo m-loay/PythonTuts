0d55e14fd4d53ae7a787a3ff1ed90ea233d8d85a
modyskyline <modyskyline@live.com>
2017-07-29 07:19:15 +0200

    adding spaces

--- PyTut4_Func/main.py
+++ PyTut4_Func/main.py
@@ -18,19 +18,25 @@ def maxValue(x,y,z):
     return maxx
 
 a=int(input("Enter First integer:"))
+
 b=int(input("Enter Second integer:"))
+
 c=int(input("Enter third integer:"))
 
 print("MAx Value= ",maxValue(a, b, c))
 
 a1=float(input("Enter First Float:"))
+
 b1=float(input("Enter Second Float:"))
+
 c1=float(input("Enter third Float:"))
 
 print("MAx Value= ",maxValue(a1, b1, c1))
 
 a2=(input("Enter First string:"))
+
 b2=(input("Enter Second string:"))
+
 c2=(input("Enter third string:"))
 
 print("MAx Value= ",maxValue(a2, b2, c2))
